24594106d9da2094857272ce4c302ed72c000bd0
ymmy2 <oleg.nosyrev@itential.com>
2023-09-19 03:28:28 -0700

    replacing resume + renaming Front End to Front-End

--- src/components/HeadBox.js
+++ src/components/HeadBox.js
@@ -1,12 +1,12 @@
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import '../styles/css/headbox.css';
-import resume from '../assets/png&jpg/Oleg_Nosyrev_Resume.pdf';
+import resume from '../assets/png&jpg/Oleg_Nosyrev.pdf';
 
 export default function HeadBox() {
   return (
     <div className='head-box'>
       <h1>Oleg Nosyrev</h1>
-      <p>Front End Software Engineer [UI/UX]</p>
+      <p>Front-End Software Engineer [UI/UX]</p>
       <a className='button-orange' href={resume} download>
         <FileDownloadIcon fontSize='medium' color='#fff' /> Resume
       </a>
