956b610992d8a6d822e10ad556d0361ffcbc7503
ymmy <chem-pion@mail.ru>
2025-10-14 18:43:05 -0700

    Finish

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,64 +1,51 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-
 import '../styles/css/entry.css';
 import '../styles/css/NavBar.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function NavBar({ main, theme, switchTheme }) {
 
   return (
-    <>
-      <Navbar id='nav' collapseOnSelect expand='md'>
-        <Navbar.Toggle />
-        <Navbar.Brand href='https://ymmy1.github.io/' className='navbar-brand'>
-          Portfolio
-        </Navbar.Brand>
-        <Navbar.Collapse>
-          <Nav className='navbar-nav ms-auto'>
-            <Nav.Item className='nav-item ml-auto'>
-              {main &&
-                (theme ? (
-                  <button
-                    className='minimalistic_button'
-                    onClick={() => switchTheme()}
-                  >
-                    <span>Minimalistic </span>
-                  </button>
-                ) : (
-                  <button
-                    className='japan_button'
-                    onClick={() => switchTheme()}
-                  >
-                    <span>Japan Theme</span>
-                  </button>
-                ))}
-            </Nav.Item>
+    <nav id='nav'>
+      <a href='https://ymmy1.github.io/' className='navbar-brand'>
+        Portfolio
+      </a>
 
-            <Nav.Item className='nav-item ml-auto'>
-              <Nav.Link href='#education'>Education</Nav.Link>
-            </Nav.Item>
-            <Nav.Item className='nav-item ml-auto'>
-              <Nav.Link href='#skills'>Skills</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href='#projects'>Projects</Nav.Link>
-            </Nav.Item>
-            <Nav.Item className='nav-item ml-auto'>
-              <Nav.Link
-                className='nav-social-link'
-                href='https://www.linkedin.com/in/oleg-nosyrev-440640114'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <LinkedInIcon fontSize='medium' />
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+      <div className='nav-items'>
+        <div className='nav-item'>
+          {main &&
+            (theme ? (
+              <button className='minimalistic_button' onClick={() => switchTheme()}>
+                <span>Minimalistic </span>
+              </button>
+            ) : (
+              <button className='japan_button' onClick={() => switchTheme()}>
+                <span>Japan Theme</span>
+              </button>
+            ))}
+        </div>
+
+        <div className='nav-item'>
+          <a href='#education'>Education</a>
+        </div>
+        <div className='nav-item'>
+          <a href='#skills'>Skills</a>
+        </div>
+        <div className='nav-item'>
+          <a href='#projects'>Projects</a>
+        </div>
+        <div className='nav-item'>
+          <a
+            className='nav-social-link'
+            href='https://www.linkedin.com/in/oleg-nosyrev-440640114'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <LinkedInIcon fontSize='medium' />
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 }
